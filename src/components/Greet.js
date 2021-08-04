87cd4bf1dc7adcfb34a5eeeb97463a45500360fe
JasonJF <47773807+JasonJF@users.noreply.github.com>
2021-08-04 14:33:52 +0200

    --react hello world

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Human</h1>
+// }
+
+// export const Greet = () => <h1>Hello Human</h1>; //named export
+const Greet = () => <h1>Hello Human</h1>; 
+
+export default Greet;
